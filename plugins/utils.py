0cd24a25c94819b1229042a5cefa83fa36881057
Vipul Sharma <vipul.sharma20@gmail.com>
2021-07-11 17:04:17 +0530

    fix: markdown doc generator
    - Fix user's name from Slack user id
    - Fix message text formatting
    - Cache user names

--- plugins/utils.py
+++ plugins/utils.py
@@ -3,6 +3,8 @@ from typing import List, Dict
 from slack.errors import SlackApiError
 from slack import WebClient
 
+from slack_doc import app_cache
+
 
 # Get channel id from channel name
 def get_channel_id(channel_name: str, client: WebClient) -> str:
@@ -28,3 +30,12 @@ def get_conversation_history(channel_id: str, parent_id: str, client: WebClient)
 
     except SlackApiError as e:
         print(f"Error: {e}")
+
+
+# Get user's name from id
+def get_user_names_from_id(user_id: str, client: WebClient) -> str:
+    if not app_cache.get(user_id):
+        user_profile = client.users_profile_get(user=user_id)
+        app_cache.set(user_id, user_profile["profile"]["real_name"])
+    return app_cache.get(user_id)
+

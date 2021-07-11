5d179af93028bc6504b034b6958cb7b7b9d229aa
Vipul Sharma <vipul.sharma20@gmail.com>
2021-07-11 17:06:22 +0530

    add: return ephemeral responses on callbacks

--- slack_doc/routes.py
+++ slack_doc/routes.py
@@ -1,28 +1,32 @@
 import json
-import traceback
 
+import requests
 from flask import current_app as app
 from flask import request, make_response
 
+import slack_doc.constants as constants
 from slack_doc.factory import PLUGINS
 
 
 @app.route("/slack/shortcut-trigger/", methods=["POST"])
 def shortcut_trigger():
-    data = request.form
+    payload = json.loads(request.form["payload"])
+    callback_id = payload["callback_id"]
+    response_url = payload["response_url"]
 
-    payload = data.get("payload")
     try:
-        payload = json.loads(payload)
-        callback_id = payload["callback_id"]
+        requests.post(response_url,
+                      json={"text": constants.EPHEMERAL_RESPONSE_ON_EXPORT,
+                            "response_type": constants.RESPONSE_TYPE_EPHEMERAL})
 
         export_plugin = PLUGINS[callback_id](payload)
         export_plugin.export_thread()
 
         return make_response("", 200)
 
-    except Exception:
-        traceback.print_exc()
+    except Exception as e:
+        requests.post(response_url,
+                      json={"text": f"Cannot create doc. Error: \n\n{e}",
+                            "response_type": constants.RESPONSE_TYPE_EPHEMERAL})
 
         return make_response("", 200)
-
